90c78f9593517a1ba015a00b7688d77285952c65
CECI\63742 <hung.che.yi@gmail.com>
2022-01-21 16:29:26 +0800

    jwt sign in

--- angular/fourSeasonWeb/src/app/app.module.ts
+++ angular/fourSeasonWeb/src/app/app.module.ts
@@ -8,24 +8,40 @@ import { ActivityTableComponent } from './activity-table/activity-table.componen
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AuthInterceptor } from './components/authconfig.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivityTableComponent
+    ActivityTableComponent,
+    SigninComponent,
+    SignupComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
